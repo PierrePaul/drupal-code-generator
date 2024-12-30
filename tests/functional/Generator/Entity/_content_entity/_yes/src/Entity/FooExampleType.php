3bdd31fc1ae327e9a3a917cd95772ef529ab187a
chesn0k <spam.yureskul@gmail.com>
2024-12-30 18:18:19 +0200

    Use PHP attributes for plugins (entity:content) #156

--- tests/functional/Generator/Entity/_content_entity/_yes/src/Entity/FooExampleType.php
+++ tests/functional/Generator/Entity/_content_entity/_yes/src/Entity/FooExampleType.php
@@ -5,52 +5,57 @@ declare(strict_types=1);
 namespace Drupal\foo\Entity;
 
 use Drupal\Core\Config\Entity\ConfigEntityBundleBase;
+use Drupal\Core\Entity\Attribute\ConfigEntityType;
+use Drupal\Core\Entity\EntityDeleteForm;
+use Drupal\Core\Entity\Routing\AdminHtmlRouteProvider;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\foo\FooExampleTypeListBuilder;
+use Drupal\foo\Form\FooExampleTypeForm;
 
 /**
  * Defines the Example type configuration entity.
- *
- * @ConfigEntityType(
- *   id = "foo_example_type",
- *   label = @Translation("Example type"),
- *   label_collection = @Translation("Example types"),
- *   label_singular = @Translation("example type"),
- *   label_plural = @Translation("examples types"),
- *   label_count = @PluralTranslation(
- *     singular = "@count examples type",
- *     plural = "@count examples types",
- *   ),
- *   handlers = {
- *     "form" = {
- *       "add" = "Drupal\foo\Form\FooExampleTypeForm",
- *       "edit" = "Drupal\foo\Form\FooExampleTypeForm",
- *       "delete" = "Drupal\Core\Entity\EntityDeleteForm",
- *     },
- *     "list_builder" = "Drupal\foo\FooExampleTypeListBuilder",
- *     "route_provider" = {
- *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
- *     },
- *   },
- *   admin_permission = "administer foo_example types",
- *   bundle_of = "foo_example",
- *   config_prefix = "foo_example_type",
- *   entity_keys = {
- *     "id" = "id",
- *     "label" = "label",
- *     "uuid" = "uuid",
- *   },
- *   links = {
- *     "add-form" = "/admin/structure/foo_example_types/add",
- *     "edit-form" = "/admin/structure/foo_example_types/manage/{foo_example_type}",
- *     "delete-form" = "/admin/structure/foo_example_types/manage/{foo_example_type}/delete",
- *     "collection" = "/admin/structure/foo_example_types",
- *   },
- *   config_export = {
- *     "id",
- *     "label",
- *     "uuid",
- *   },
- * )
  */
+#[ConfigEntityType(
+  id: 'foo_example_type',
+  label: new TranslatableMarkup('Example type'),
+  label_collection: new TranslatableMarkup('Example types'),
+  label_singular: new TranslatableMarkup('example type'),
+  label_plural: new TranslatableMarkup('examples types'),
+  config_prefix: 'foo_example_type',
+  entity_keys: [
+    'id' => 'id',
+    'label' => 'label',
+    'uuid' => 'uuid',
+  ],
+  handlers: [
+    'list_builder' => FooExampleTypeListBuilder::class,
+    'route_provider' => [
+      'html' => AdminHtmlRouteProvider::class,
+    ],
+    'form' => [
+      'add' => FooExampleTypeForm::class,
+      'edit' => FooExampleTypeForm::class,
+      'delete' => EntityDeleteForm::class,
+    ],
+  ],
+  links: [
+    'add-form' => '/admin/structure/foo_example_types/add',
+    'edit-form' => '/admin/structure/foo_example_types/manage/{foo_example_type}',
+    'delete-form' => '/admin/structure/foo_example_types/manage/{foo_example_type}/delete',
+    'collection' => '/admin/structure/foo_example_types',
+  ],
+  admin_permission: 'administer foo_example types',
+  bundle_of: 'foo_example',
+  label_count: [
+    'singular' => '@count example type',
+    'plural' => '@count examples types',
+  ],
+  config_export: [
+    'id',
+    'label',
+    'uuid',
+  ],
+)]
 final class FooExampleType extends ConfigEntityBundleBase {
 
   /**
