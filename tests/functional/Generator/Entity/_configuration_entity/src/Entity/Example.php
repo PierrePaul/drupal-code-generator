96c27d6de9effc4c0037ef41ac0625de15c9bd0d
chesn0k <spam.yureskul@gmail.com>
2024-12-30 15:36:33 +0200

    Use PHP attributes for plugins (entity:configuration) #156

--- tests/functional/Generator/Entity/_configuration_entity/src/Entity/Example.php
+++ tests/functional/Generator/Entity/_configuration_entity/src/Entity/Example.php
@@ -5,49 +5,53 @@ declare(strict_types=1);
 namespace Drupal\foo\Entity;
 
 use Drupal\Core\Config\Entity\ConfigEntityBase;
+use Drupal\Core\Entity\Attribute\ConfigEntityType;
+use Drupal\Core\Entity\EntityDeleteForm;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\foo\ExampleInterface;
+use Drupal\foo\ExampleListBuilder;
+use Drupal\foo\Form\ExampleForm;
 
 /**
  * Defines the example entity type.
- *
- * @ConfigEntityType(
- *   id = "example",
- *   label = @Translation("Example"),
- *   label_collection = @Translation("Examples"),
- *   label_singular = @Translation("example"),
- *   label_plural = @Translation("examples"),
- *   label_count = @PluralTranslation(
- *     singular = "@count example",
- *     plural = "@count examples",
- *   ),
- *   handlers = {
- *     "list_builder" = "Drupal\foo\ExampleListBuilder",
- *     "form" = {
- *       "add" = "Drupal\foo\Form\ExampleForm",
- *       "edit" = "Drupal\foo\Form\ExampleForm",
- *       "delete" = "Drupal\Core\Entity\EntityDeleteForm",
- *     },
- *   },
- *   config_prefix = "example",
- *   admin_permission = "administer example",
- *   links = {
- *     "collection" = "/admin/structure/example",
- *     "add-form" = "/admin/structure/example/add",
- *     "edit-form" = "/admin/structure/example/{example}",
- *     "delete-form" = "/admin/structure/example/{example}/delete",
- *   },
- *   entity_keys = {
- *     "id" = "id",
- *     "label" = "label",
- *     "uuid" = "uuid",
- *   },
- *   config_export = {
- *     "id",
- *     "label",
- *     "description",
- *   },
- * )
  */
+#[ConfigEntityType(
+  id: 'example',
+  label: new TranslatableMarkup('Example'),
+  label_collection: new TranslatableMarkup('Examples'),
+  label_singular: new TranslatableMarkup('example'),
+  label_plural: new TranslatableMarkup('examples'),
+  config_prefix: 'example',
+  entity_keys: [
+    'id' => 'id',
+    'label' => 'label',
+    'uuid' => 'uuid',
+  ],
+  handlers: [
+    'list_builder' => ExampleListBuilder::class,
+    'form' => [
+      'add' => ExampleForm::class,
+      'edit' => ExampleForm::class,
+      'delete' => EntityDeleteForm::class,
+    ],
+  ],
+  links: [
+    'collection' => '/admin/structure/example',
+    'add-form' => '/admin/structure/example/add',
+    'edit-form' => '/admin/structure/example/{example}',
+    'delete-form' => '/admin/structure/example/{example}/delete',
+  ],
+  admin_permission: 'administer example',
+  label_count: [
+    'singular' => '@count example',
+    'plural' => '@count examples',
+  ],
+  config_export: [
+    'id',
+    'label',
+    'description',
+  ],
+)]
 final class Example extends ConfigEntityBase implements ExampleInterface {
 
   /**
